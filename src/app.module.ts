1fd574a53a54852b8198746c9c073f97978625fa
SushWar <130230991+SushWar@users.noreply.github.com>
2023-11-02 10:57:56 +0000

    Configue schdule module and PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { NotificationService } from './notification/notification.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database/database.service';
 import { AdminModule } from './admin/admin.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TelegramclientModule, AdminModule],
+  imports: [ConfigModule.forRoot(),ScheduleModule.forRoot(), TelegramclientModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, NotificationService, DatabaseService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-const port = process.env.PORT || 7000
+// const port = process.env.PORT || 7000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') || 5555;
   await app.listen(port);
 }
 bootstrap();
